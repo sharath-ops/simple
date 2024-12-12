5d87bf6721f68c730ce44753e82ecc571c26ad55
sharath reddy <sharath111reddy@gmail.com>
2024-12-13 00:37:31 +0530

    function defined

--- count.js
+++ count.js
@@ -1,6 +1,9 @@
 let x = 0
-
-for(let i = 0 ; i < 20 ; i++){
-    x = x + 1
-}   
+function mul(y){
+    for(let i = 0 ; i < 10 ; i++){
+        y = y + 1
+    }
+    return y
+}
+x = mul(0)
 console.log(x)
